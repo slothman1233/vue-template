5ddbad6fe7dc9bb6c5c6ce21c4a4e9cb0739156d
EveChee <evec@vip.qq.com>
2020-06-19 13:54:51 +0800

    加载最新的stlmfe css设置

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+const APP_NAME = require('package.json').name
 module.exports = () => {
   return {
-    plugins: [
-      require('postcss-plugin-namespace')('#huihun-admin', { ignore: [/^\.el-/, /^[^\.]/, /^#/] }),
+    plugins:[
+      require('postcss-plugin-namespace')(`#${APP_NAME}`, { ignore: [ /^\.el-/ , `#${APP_NAME}`] }),
       require('autoprefixer')
     ],
   }
